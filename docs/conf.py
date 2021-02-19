c3542d1c68e2f336ee568f2f8470ced75c875b5e
Hagen Wierstorf <hwierstorf@audeering.com>
2021-02-19 10:11:04 +0100

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,57 @@
+import configparser
+from datetime import date
+import os
+
+import audb_artifactory
+
+
+config = configparser.ConfigParser()
+config.read(os.path.join('..', 'setup.cfg'))
+
+# Project -----------------------------------------------------------------
+author = config['metadata']['author']
+copyright = f'2021-{date.today().year} audEERING GmbH'
+project = config['metadata']['name']
+version = audb_artifactory.__version__
+title = f'{project} Documentation'
+
+
+# General -----------------------------------------------------------------
+master_doc = 'index'
+extensions = []
+source_suffix = '.rst'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+pygments_style = None
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # support for Google-style docstrings
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
+]
+
+napoleon_use_ivar = True  # List of class attributes
+autodoc_inherit_docstrings = False  # disable docstring inheritance
+intersphinx_mapping = {
+    'audeer': ('https://audeering.github.io/audeer/', None),
+    'audb2': ('http://tools.pp.audeering.com/audb2/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
+    'python': ('https://docs.python.org/3/', None),
+}
+linkcheck_ignore = [
+    'https://gitlab.audeering.com',
+]
+# Ignore package dependencies during building the docs
+# This fixes URL link issues with pandas and sphinx_autodoc_typehints
+autodoc_mock_imports = [
+    'pandas',
+]
+
+# HTML --------------------------------------------------------------------
+html_theme = 'sphinx_audeering_theme'
+html_theme_options = {
+    'display_version': True,
+    'logo_only': False,
+}
+html_title = title
