8ff4cec533ce208f8252283d442a8bf236b96f96
Johannes Wagner <jwagner@audeering.com>
2022-12-08 10:06:09 +0100

    DOC: add docstring examples

---
+++ audbackend/core/conftest.py
@@ -0,0 +1,23 @@
+import tempfile
+import pytest
+
+import audbackend
+import audeer
+
+
+@pytest.fixture(autouse=True)
+def create_backend(doctest_namespace):
+    with tempfile.TemporaryDirectory() as tmp:
+        backend = audbackend.create(
+            'file-system',
+            tmp,
+            'doctest',
+        )
+        src_file = 'src.pth'
+        src_path = audeer.touch(audeer.path(tmp, src_file))
+        backend.put_archive(tmp, [src_file], 'folder/name', '1.0.0')
+        for version in ['1.0.0', '2.0.0']:
+            backend.put_file(src_path, 'folder/name.ext', version)
+        doctest_namespace['backend'] = backend
+        doctest_namespace['tmp'] = tmp
+        yield
