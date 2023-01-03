551ac82b11ce4c1d2b4b25ee5d794542b5196c61
Hagen Wierstorf <hwierstorf@audeering.com>
2023-01-03 17:42:21 +0100

    DOC: split up into sub-pages per class/function (#68)
    * DOC: split up into sub-pages per class/function
    
    * Rename Exampl to Examples

--- docs/conf.py
+++ docs/conf.py
@@ -1,30 +1,40 @@
 import configparser
 from datetime import date
 import os
+import shutil
 
 import audbackend
+import audeer
 
 
 config = configparser.ConfigParser()
 config.read(os.path.join('..', 'setup.cfg'))
+
 
 # Project -----------------------------------------------------------------
 author = config['metadata']['author']
 copyright = f'2021-{date.today().year} audEERING GmbH'
 project = config['metadata']['name']
 version = audbackend.__version__
-title = f'{project} Documentation'
+title = 'Documentation'
 
 
 # General -----------------------------------------------------------------
 master_doc = 'index'
-extensions = []
 source_suffix = '.rst'
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+exclude_patterns = [
+    'api-src',
+    'build',
+    'tests',
+    'Thumbs.db',
+    '.DS_Store',
+]
+templates_path = ['_templates']
 pygments_style = None
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',  # support for Google-style docstrings
+    'sphinx.ext.autosummary',
     'sphinx_autodoc_typehints',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
@@ -47,6 +57,11 @@ autodoc_mock_imports = [
     'pandas',
 ]
 
+# Disable auto-generation of TOC entries in the API
+# https://github.com/sphinx-doc/sphinx/issues/6316
+toc_object_entries = False
+
+
 # HTML --------------------------------------------------------------------
 html_theme = 'sphinx_audeering_theme'
 html_theme_options = {
@@ -58,3 +73,15 @@ html_context = {
     'display_github': True,
 }
 html_title = title
+
+
+# Copy API (sub-)module RST files to docs/api/ folder ---------------------
+audeer.rmdir('api')
+audeer.mkdir('api')
+api_src_files = audeer.list_file_names('api-src')
+api_dst_files = [
+    audeer.path('api', os.path.basename(src_file))
+    for src_file in api_src_files
+]
+for src_file, dst_file in zip(api_src_files, api_dst_files):
+    shutil.copyfile(src_file, dst_file)
