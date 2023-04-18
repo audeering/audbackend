9dd0aacbfcb72a3180968b0ba4d1ed2bb6b06178
Johannes Wagner <jwagner@audeering.com>
2023-04-18 15:41:45 +0200

    TST: test BackendError with all functions

--- tests/test_artifactory.py
+++ tests/test_artifactory.py
@@ -10,14 +10,32 @@ BACKEND = audbackend.Artifactory(
 )
 
 
-def test_exists(no_artifactory_access_rights):
+def test_errors(tmpdir, no_artifactory_access_rights):
+
+    local_file = audeer.touch(
+        audeer.path(tmpdir, 'file.txt')
+    )
     remote_file = BACKEND.join(
         audeer.uid()[:8],
         'file.txt',
     )
     version = '1.0.0'
+
     with pytest.raises(audbackend.BackendError):
         BACKEND.exists(remote_file, version)
+
+    with pytest.raises(audbackend.BackendError):
+        BACKEND.put_file(
+            local_file,
+            remote_file,
+            version,
+        )
+
+    with pytest.raises(audbackend.BackendError):
+        BACKEND.latest_version(remote_file)
+
+    with pytest.raises(audbackend.BackendError):
+        BACKEND.versions(remote_file)
 
 
 # TODO: re-enable once we have solved
