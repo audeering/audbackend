bc9f6e6c599fb3d06e2c9e3b4475bf057403eb8f
Hagen Wierstorf <hwierstorf@audeering.com>
2023-04-24 15:58:17 +0200

    TST: clean up Artifactory unit test repositories

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 import audeer
+import audfactory
 
 import audbackend
 
@@ -23,16 +24,42 @@ pytest.BACKENDS = [
 
 @pytest.fixture(scope='function', autouse=False)
 def backend(request):
+    r"""Create and delete a repository on the backend.
 
+    Deleting a repository might fail on Artifactory hosts,
+    hence we catch the error
+    and instead look for leftover repositories
+    at the beginning and clean try to clean them.
+
+    """
     name = request.param
     host = pytest.HOSTS[name]
     repository = f'unittest-{audeer.uid()[:8]}'
+
+    # Clean up possible left over repos on Artifactory host
+    if name == 'artifactory':
+        r = audfactory.rest_api_get(f'{host}/api/repositories')
+        if r.status_code == 200:
+            repos = [entry['key'] for entry in r.json()]
+            repos = [
+                repo for repo in repos
+                if repo != 'unittests-public' and repo.startswith('unittest-')
+            ]
+            for repo in repos:
+                try:
+                    audbackend.delete(name, host, repo)
+                except audbackend.BackendError:
+                    pass
 
     backend = audbackend.create(name, host, repository)
 
     yield backend
 
-    audbackend.delete(name, host, repository)
+    # Deleting repositories on Artifactory might fail
+    try:
+        audbackend.delete(name, host, repository)
+    except audbackend.BackendError:
+        pass
 
 
 @pytest.fixture(scope='package', autouse=True)
