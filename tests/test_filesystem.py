798b5dc424a868159c71d4c6bf3a7c05bc7bf29d
Johannes Wagner <jwagner@audeering.com>
2023-09-08 09:51:03 +0200

    TST: test legacy file system

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 import audeer
@@ -51,3 +53,35 @@ def test_get_file_interrupt(tmpdir, bad_file_system, backend):
     with pytest.raises(audbackend.BackendError):
         backend.get_file('/file', src_path, '1.0.0')
     assert audeer.md5(src_path) == checksum_local
+
+
+@pytest.mark.parametrize(
+    'backend',
+    ['file-system'],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    'file, version, extensions, expected',
+    [
+        ('/file.tar.gz', '1.0.0', None, 'file.tar/1.0.0/file.tar-1.0.0.gz'),
+        ('/file.tar.gz', '1.0.0', [], 'file.tar/1.0.0/file.tar-1.0.0.gz'),
+        ('/file.tar.gz', '1.0.0', ['tar.gz'], 'file/1.0.0/file-1.0.0.tar.gz'),
+        ('/.tar.gz', '1.0.0', ['tar.gz'], '.tar/1.0.0/.tar-1.0.0.gz'),
+        ('/tar.gz', '1.0.0', ['tar.gz'], 'tar/1.0.0/tar-1.0.0.gz'),
+        ('/.tar.gz', '1.0.0', None, '.tar/1.0.0/.tar-1.0.0.gz'),
+        ('/.tar', '1.0.0', None, '.tar/1.0.0/.tar-1.0.0'),
+        ('/tar', '1.0.0', None, 'tar/1.0.0/tar-1.0.0'),
+    ]
+)
+def test_legacy_file_structure(tmpdir, backend, file, version, extensions,
+                               expected):
+
+    backend._use_legacy_file_structure(extensions=extensions)
+
+    src_path = audeer.touch(audeer.path(tmpdir, 'tmp'))
+    backend.put_file(src_path, file, version)
+
+    path = os.path.join(backend._root, expected)
+    assert str(backend._path(file, version)) == path
+    assert backend.ls(file) == [(file, version)]
+    assert backend.ls() == [(file, version)]
