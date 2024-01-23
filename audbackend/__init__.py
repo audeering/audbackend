acbbcf02fd832c198cc5361bce3b8dc60f0019e8
Johannes Wagner <jwagner@aurismatic.com>
2024-01-23 17:40:08 +0100

    legacy imports

--- audbackend/__init__.py
+++ audbackend/__init__.py
@@ -5,16 +5,16 @@ from audbackend.core.api import available
 from audbackend.core.api import create
 from audbackend.core.api import delete
 from audbackend.core.api import register
-# from audbackend.core.backend.base import Base as Backend
-# from audbackend.core.backend.filesystem import FileSystem
+from audbackend.core.backend.base import Base as Backend  # legacy
+from audbackend.core.backend.filesystem import FileSystem  # legacy
 from audbackend.core.errors import BackendError
 from audbackend.core.repository import Repository
 
-# Import optional backends
-# try:
-#     from audbackend.core.backend.artifactory import Artifactory
-# except ImportError:  # pragma: no cover
-#     pass
+# Import optional backends (legacy)
+try:
+    from audbackend.core.backend.artifactory import Artifactory
+except ImportError:  # pragma: no cover
+    pass
 
 
 __all__ = []
