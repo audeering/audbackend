33ec848ac97450c161d7fdc308966ddd16156e1d
Johannes Wagner <jwagner@audeering.com>
2024-03-01 15:40:11 +0100

    TST: Make single folder backend work on Windows (#191)
    * DEBUG: apply some dummy change
    
    * TST: test on windows only
    
    * TST: test on windows only
    
    * TST: test single-folder backend on Windows
    
    * TST: test only single-folder backend
    
    * TST: register single-folder backend on Windows
    
    * TST: debug why .map file does not exist
    
    * TST: replace shelve with pickle in single folder backend
    
    * TST: test windows only
    
    * TST: enable tests on all platforms
    
    * TST: cover default copy and move implementation on Windows

--- audbackend/core/backend/base.py
+++ audbackend/core/backend/base.py
@@ -76,7 +76,7 @@ class Base:
             src_path: str,
             dst_path: str,
             verbose: bool,
-    ):  # pragma: no Windows cover
+    ):
         r"""Copy file on backend.
 
         A default implementation is provided,
@@ -509,7 +509,7 @@ class Base:
             src_path: str,
             dst_path: str,
             verbose: bool,
-    ):  # pragma: no Windows cover
+    ):
         r"""Move file on backend.
 
         A default implementation is provided,
