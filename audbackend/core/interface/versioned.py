8c6200b5a1c0730e90edaa5b03dab810e59dff88
Johannes Wagner <jwagner@audeering.com>
2024-03-05 14:37:37 +0100

    Introduce optional validation step (#190)
    * add validate argument to backend.Base.[copy,get,move,put]_file
    
    * backend.Base.move_file(): improve docstring
    
    * DOC: fix typo
    
    * audbackend.Base.[get,put]_archive(): add validate argument
    
    * DOC: add missing rasises section to backend.base.put_archive()
    
    * interface.Unversioned: add validate argument
    
    * interface.Versioned: add validate argument
    
    * interface.Versioned: fix where validate was not passed
    
    * backend.Base.move_file(): rely on copy_file if validate is True
    
    * TST: update
    
    * backend.Base: make _assert_equal_checksum() keyword only

--- audbackend/core/interface/versioned.py
+++ audbackend/core/interface/versioned.py
@@ -66,6 +66,7 @@ class Versioned(Base):
             dst_path: str,
             *,
             version: str = None,
+            validate: bool = False,
             verbose: bool = False,
     ):
         r"""Copy file on backend.
@@ -80,9 +81,18 @@ class Versioned(Base):
         Otherwise,
         the operation is silently skipped.
 
+        If ``validate`` is set to ``True``,
+        a final check is performed to assert that
+        ``src_path`` and ``dst_path``
+        have the same checksum.
+        If it fails,
+        ``dst_path`` is removed and
+        an :class:`InterruptedError` is raised.
+
         Args:
             src_path: source path to file on backend
             dst_path: destination path to file on backend
+            validate: verify file was successfully copied
             version: version string
             verbose: show debug messages
 
@@ -95,6 +105,7 @@ class Versioned(Base):
 
         Raises:
             BackendError: if an error is raised on the backend
+            InterruptedError: if validation fails
             ValueError: if ``src_path`` or ``dst_path``
                 does not start with ``'/'`` or
                 does not match ``'[A-Za-z0-9/._-]+'``
@@ -113,6 +124,7 @@ class Versioned(Base):
             self.backend.copy_file(
                 src_path_with_version,
                 dst_path_with_version,
+                validate=validate,
                 verbose=verbose,
             )
 
@@ -195,6 +207,7 @@ class Versioned(Base):
             version: str,
             *,
             tmp_root: str = None,
+            validate: bool = False,
             verbose: bool = False,
     ) -> typing.List[str]:
         r"""Get archive from backend and extract.
@@ -205,12 +218,22 @@ class Versioned(Base):
         If ``dst_root`` does not exist,
         it is created.
 
+        If ``validate`` is set to ``True``,
+        a final check is performed to assert that
+        ``src_path`` and the retrieved archive
+        have the same checksum.
+        If it fails,
+        the retrieved archive is removed and
+        an :class:`InterruptedError` is raised.
+
         Args:
             src_path: path to archive on backend
             dst_root: local destination directory
             version: version string
             tmp_root: directory under which archive is temporarily extracted.
                 Defaults to temporary directory of system
+            validate: verify archive was successfully
+                retrieved from the backend
             verbose: show debug messages
 
         Returns:
@@ -220,6 +243,7 @@ class Versioned(Base):
             BackendError: if an error is raised on the backend,
                 e.g. ``src_path`` does not exist
             FileNotFoundError: if ``tmp_root`` does not exist
+            InterruptedError: if validation fails
             NotADirectoryError: if ``dst_root`` is not a directory
             PermissionError: if the user lacks write permissions
                 for ``dst_path``
@@ -240,6 +264,7 @@ class Versioned(Base):
             src_path_with_version,
             dst_root,
             tmp_root=tmp_root,
+            validate=validate,
             verbose=verbose,
         )
 
@@ -249,6 +274,7 @@ class Versioned(Base):
             dst_path: str,
             version: str,
             *,
+            validate: bool = False,
             verbose: bool = False,
     ) -> str:
         r"""Get file from backend.
@@ -263,14 +289,20 @@ class Versioned(Base):
         or otherwise,
         the operation is silently skipped.
 
-        To ensure the file is completely retrieved,
-        it is first stored in a temporary directory
-        and afterwards moved to ``dst_path``.
+        If ``validate`` is set to ``True``,
+        a final check is performed to assert that
+        ``src_path`` and ``dst_path``
+        have the same checksum.
+        If it fails,
+        ``dst_path`` is removed and
+        an :class:`InterruptedError` is raised.
 
         Args:
             src_path: path to file on backend
             dst_path: destination path to local file
             version: version string
+            validate: verify file was successfully
+                retrieved from the backend
             verbose: show debug messages
 
         Returns:
@@ -279,6 +311,7 @@ class Versioned(Base):
         Raises:
             BackendError: if an error is raised on the backend,
                 e.g. ``src_path`` does not exist
+            InterruptedError: if validation fails
             IsADirectoryError: if ``dst_path`` points to an existing folder
             PermissionError: if the user lacks write permissions
                 for ``dst_path``
@@ -299,6 +332,7 @@ class Versioned(Base):
         return self.backend.get_file(
             src_path_with_version,
             dst_path,
+            validate=validate,
             verbose=verbose,
         )
 
@@ -489,6 +523,7 @@ class Versioned(Base):
             dst_path: str,
             *,
             version: str = None,
+            validate: bool = False,
             verbose: bool = False,
     ):
         r"""Move file on backend.
@@ -504,17 +539,32 @@ class Versioned(Base):
         ``src_path``
         is removed and the operation silently skipped.
 
+        If ``validate`` is set to ``True``,
+        a final check is performed to assert that
+        ``src_path`` and ``dst_path``
+        have the same checksum.
+        If it fails,
+        ``dst_path`` is removed and
+        an :class:`InterruptedError` is raised.
+        To ensure ``src_path`` still exists in this case
+        it is first copied and only removed
+        when the check has successfully passed.
+
         Args:
             src_path: source path to file on backend
             dst_path: destination path to file on backend
             version: version string
+            validate: verify file was successfully moved
             verbose: show debug messages
 
         Raises:
             BackendError: if an error is raised on the backend
+            InterruptedError: if validation fails
             ValueError: if ``src_path`` or ``dst_path``
                 does not start with ``'/'`` or
                 does not match ``'[A-Za-z0-9/._-]+'``
+            ValueError: if ``version`` is empty or
+                does not match ``'[A-Za-z0-9._-]+'``
 
         Examples:
             >>> versioned.exists('/move.ext', '1.0.0')
@@ -524,14 +574,6 @@ class Versioned(Base):
             True
             >>> versioned.exists('/f.ext', '1.0.0')
             False
-
-        Raises:
-            BackendError: if an error is raised on the backend
-            ValueError: if ``src_path`` or ``dst_path``
-                does not start with ``'/'`` or
-                does not match ``'[A-Za-z0-9/._-]+'``
-            ValueError: if ``version`` is empty or
-                does not match ``'[A-Za-z0-9._-]+'``
 
         """
         if version is None:
@@ -545,6 +587,7 @@ class Versioned(Base):
             self.backend.move_file(
                 src_path_with_version,
                 dst_path_with_version,
+                validate=validate,
                 verbose=verbose,
             )
 
@@ -590,6 +633,7 @@ class Versioned(Base):
             *,
             files: typing.Union[str, typing.Sequence[str]] = None,
             tmp_root: str = None,
+            validate: bool = False,
             verbose: bool = False,
     ):
         r"""Create archive and put on backend.
@@ -600,6 +644,14 @@ class Versioned(Base):
         The operation is silently skipped,
         if an archive with the same checksum
         already exists on the backend.
+
+        If ``validate`` is set to ``True``,
+        a final check is performed to assert that
+        the local archive and ``dst_path``
+        have the same checksum.
+        If it fails,
+        ``dst_path`` is removed and
+        an :class:`InterruptedError` is raised.
 
         Args:
             src_root: local root directory where files are located.
@@ -613,6 +665,8 @@ class Versioned(Base):
                 Must exist within ``src_root``
             tmp_root: directory under which archive is temporarily created.
                 Defaults to temporary directory of system
+            validate: verify archive was successfully
+                put on the backend
             verbose: show debug messages
 
         Raises:
@@ -620,6 +674,7 @@ class Versioned(Base):
             FileNotFoundError: if ``src_root``,
                 ``tmp_root``,
                 or one or more ``files`` do not exist
+            InterruptedError: if validation fails
             NotADirectoryError: if ``src_root`` is not a folder
             RuntimeError: if ``dst_path`` does not end with
                 ``zip`` or ``tar.gz``
@@ -643,6 +698,7 @@ class Versioned(Base):
             dst_path_with_version,
             files=files,
             tmp_root=tmp_root,
+            validate=validate,
             verbose=verbose,
         )
 
@@ -652,6 +708,7 @@ class Versioned(Base):
             dst_path: str,
             version: str,
             *,
+            validate: bool = False,
             verbose: bool = False,
     ):
         r"""Put file on backend.
@@ -660,10 +717,20 @@ class Versioned(Base):
         if a file with the same checksum
         already exists on the backend.
 
+        If ``validate`` is set to ``True``,
+        a final check is performed to assert that
+        ``src_path`` and ``dst_path``
+        have the same checksum.
+        If it fails,
+        ``dst_path`` is removed and
+        an :class:`InterruptedError` is raised.
+
         Args:
             src_path: path to local file
             dst_path: path to file on backend
             version: version string
+            validate: verify file was successfully
+                put on the backend
             verbose: show debug messages
 
         Returns:
@@ -672,6 +739,7 @@ class Versioned(Base):
         Raises:
             BackendError: if an error is raised on the backend
             FileNotFoundError: if ``src_path`` does not exist
+            InterruptedError: if validation fails
             IsADirectoryError: if ``src_path`` is a folder
             ValueError: if ``dst_path`` does not start with ``'/'`` or
                 does not match ``'[A-Za-z0-9/._-]+'``
@@ -690,6 +758,7 @@ class Versioned(Base):
         return self.backend.put_file(
             src_path,
             dst_path_with_version,
+            validate=validate,
             verbose=verbose,
         )
 
