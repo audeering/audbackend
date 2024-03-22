d703e5db2116cad6481a0b7f533e7e6567cff419
Hagen Wierstorf <hwierstorf@audeering.com>
2024-03-22 15:07:06 +0100

    Expand test for error messages

--- tests/test_interface_maven.py
+++ tests/test_interface_maven.py
@@ -1,10 +1,71 @@
 import os
+import re
+import stat
 
 import pytest
 
 import audeer
 
 import audbackend
+
+
+@pytest.mark.parametrize(
+    "interface",
+    [("file-system", audbackend.interface.Maven)],
+    indirect=True,
+)
+def test_errors(tmpdir, interface):
+    # Ensure we have one file and one archive published on the backend
+    archive = "/archive.zip"
+    local_file = "file.txt"
+    local_path = audeer.touch(audeer.path(tmpdir, local_file))
+    remote_file = f"/{local_file}"
+    version = "1.0.0"
+    interface.put_file(local_path, remote_file, version)
+    interface.put_archive(tmpdir, archive, version, files=[local_file])
+
+    # Create local read-only file and folder
+    file_read_only = audeer.touch(audeer.path(tmpdir, "read-only-file.txt"))
+    os.chmod(file_read_only, stat.S_IRUSR)
+    folder_read_only = audeer.mkdir(audeer.path(tmpdir, "read-only-folder"))
+    os.chmod(folder_read_only, stat.S_IRUSR)
+
+    # Invalid file names / versions and error messages
+    file_invalid_path = "invalid/path.txt"
+    error_invalid_path = re.escape(
+        f"Invalid backend path '{file_invalid_path}', " f"must start with '/'."
+    )
+    file_invalid_char = "/invalid/char.txt?"
+    error_invalid_char = re.escape(
+        f"Invalid backend path '{file_invalid_char}', "
+        f"does not match '[A-Za-z0-9/._-]+'."
+    )
+    error_backend = (
+        "An exception was raised by the backend, "
+        "please see stack trace for further information."
+    )
+
+    # --- ls ---
+    # `path` does not exist
+    with pytest.raises(audbackend.BackendError, match=error_backend):
+        interface.ls("/missing/")
+    interface.ls("/missing/", suppress_backend_errors=True)
+    with pytest.raises(audbackend.BackendError, match=error_backend):
+        interface.ls("/missing.txt")
+    interface.ls("/missing.txt", suppress_backend_errors=True)
+    remote_file_with_wrong_ext = audeer.replace_file_extension(
+        remote_file,
+        "missing",
+    )
+    with pytest.raises(audbackend.BackendError, match=error_backend):
+        interface.ls(remote_file_with_wrong_ext)
+    interface.ls(remote_file_with_wrong_ext, suppress_backend_errors=True)
+    # joined path without leading '/'
+    with pytest.raises(ValueError, match=error_invalid_path):
+        interface.ls(file_invalid_path)
+    # `path` contains invalid character
+    with pytest.raises(ValueError, match=error_invalid_char):
+        interface.ls(file_invalid_char)
 
 
 @pytest.mark.parametrize(
