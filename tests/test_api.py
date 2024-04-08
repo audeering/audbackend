d94b1241335735b0883d50d4adbf11f7d2752f2a
Hagen Wierstorf <hwierstorf@audeering.com>
2024-04-08 09:26:10 +0200

    Try to fix warning matching

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,4 @@
 import pytest
-import warning
 
 import audeer
 
@@ -48,10 +47,22 @@ def test_api(hosts, name, host, repository, cls):
     if host is not None and host in hosts:
         host = hosts[name]
 
-    error_msg = "A backend class with name 'bad' does not exist."
+    access_warning = (
+        "access is deprecated and will be removed with version 2.2.0. "
+        r"Use Backend.__init__\(\) of corresponding backend instead."
+    )
+    create_warning = (
+        "create is deprecated and will be removed with version 2.2.0. "
+        r"Use class method Backend.create\(\) of corresponding backend instead."
+    )
+    delete_warning = (
+        "delete is deprecated and will be removed with version 2.2.0. "
+        r"Use class method Backend.delete\(\) of corresponding backend instead."
+    )
 
+    error_msg = "A backend class with name 'bad' does not exist."
     with pytest.raises(ValueError, match=error_msg):
-        with pytest.warns(UserWarning, match=warning):
+        with pytest.warns(UserWarning, match=access_warning):
             audbackend.access("bad", host, repository)
 
     error_msg = (
@@ -60,34 +71,26 @@ def test_api(hosts, name, host, repository, cls):
     )
 
     with pytest.raises(audbackend.BackendError, match=error_msg):
-        with pytest.warns(UserWarning, match=warning):
+        with pytest.warns(UserWarning, match=access_warning):
             audbackend.access(name, host, repository)
 
     # returns versioned interface for legacy reasons
-    warning = (
-        "create is deprecated and will be removed with version 2.2.0. "
-        r"Use class method Backend.create\(\) of corresponding backend instead."
-    )
-    with pytest.warns(UserWarning, match=warning):
+    with pytest.warns(UserWarning, match=create_warning):
         interface = audbackend.create(name, host, repository)
     assert isinstance(interface, audbackend.interface.Versioned)
     assert isinstance(interface.backend, cls)
 
     with pytest.raises(audbackend.BackendError, match=error_msg):
-        with pytest.warns(UserWarning, match=warning):
+        with pytest.warns(UserWarning, match=create_warning):
             audbackend.create(name, host, repository)
 
-    with pytest.warns(UserWarning, match=warning):
+    with pytest.warns(UserWarning, match=access_warning):
         interface = audbackend.access(name, host, repository)
     assert isinstance(interface.backend, cls)
 
-    warning = (
-        "delete is deprecated and will be removed with version 2.2.0. "
-        r"Use class method Backend.delete\(\) of corresponding backend instead."
-    )
-    with pytest.warns(UserWarning, match=warning):
+    with pytest.warns(UserWarning, match=delete_warning):
         audbackend.delete(name, host, repository)
 
     with pytest.raises(audbackend.BackendError, match=error_msg):
-        with pytest.warns(UserWarning, match=warning):
+        with pytest.warns(UserWarning, match=access_warning):
             audbackend.access(name, host, repository)
