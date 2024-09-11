47874bbb9ac1d6620212e00e27e93c17e28f3f17
Hagen Wierstorf <hwierstorf@audeering.com>
2024-09-11 09:45:39 +0200

    Remove unneeded test files

--- tests/test_backend_filesystem_only.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-import audbackend
-
-
-# Check optional backends are not available
-with pytest.raises(AttributeError):
-    audbackend.backend.Artifactory("https://host.com", "repo")
