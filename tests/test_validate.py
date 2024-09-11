ae2f399b2ea4820ba27e17dd56a7a00e71754d20
Hagen Wierstorf <hwierstorf@audeering.com>
2024-09-11 15:11:43 +0200

    Improve structure of validate tests

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -9,7 +9,11 @@ import audeer
 import audbackend
 
 
-def test_validate(tmpdir, filesystem):
+# def test_validate(tmpdir, filesystem):
+
+
+@pytest.mark.parametrize("error_msg", ["Execution is interrupted because"])
+class TestValidate:
     """Test methods that provide validation.
 
     We inject a broken `checksum()` method
@@ -22,113 +26,79 @@ def test_validate(tmpdir, filesystem):
         filesystem: filesystemn fixture
 
     """
-    backend = audbackend.Versioned(filesystem)
 
-    def random_checksum(path: str) -> str:
-        r"""Return random checksum."""
-        return "".join(
-            random.choices(
-                string.ascii_uppercase + string.digits,
-                k=33,
+    @classmethod
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(cls, tmpdir, filesystem, monkeypatch):
+        """Instantiate backend objects and local file."""
+        cls.backend = audbackend.Unversioned(filesystem)
+
+        def random_checksum(path: str) -> str:
+            r"""Return random checksum."""
+            return "".join(
+                random.choices(
+                    string.ascii_uppercase + string.digits,
+                    k=33,
+                )
             )
-        )
 
-    broken_backend = audbackend.Versioned(filesystem)
-    broken_backend._checksum = random_checksum
+        cls.broken_backend = audbackend.Unversioned(filesystem)
+        cls.broken_backend._checksum = random_checksum
+        cls.local_file = audeer.touch(tmpdir, "~.txt")
 
-    path = audeer.touch(tmpdir, "~.txt")
-    error_msg = "Execution is interrupted because"
+        working_dir = audeer.mkdir(tmpdir, "work")
+        monkeypatch.chdir(working_dir)
 
-    # put_file()
-    with pytest.raises(InterruptedError, match=error_msg):
-        broken_backend.put_file(path, "/remote.txt", "1.0.0", validate=True)
-    assert not backend.exists("/remote.txt", "1.0.0")
-    backend.put_file(path, "/remote.txt", "1.0.0", validate=True)
-    assert backend.exists("/remote.txt", "1.0.0")
+    def test_put_file(self, error_msg):
+        """Test validate for put_file."""
+        with pytest.raises(InterruptedError, match=error_msg):
+            self.broken_backend.put_file(self.local_file, "/remote.txt", validate=True)
+        assert not self.backend.exists("/remote.txt")
+        self.backend.put_file(self.local_file, "/remote.txt", validate=True)
+        assert self.backend.exists("/remote.txt")
 
-    # get_file()
-    with pytest.raises(InterruptedError, match=error_msg):
-        broken_backend.get_file(
-            "/remote.txt",
-            "local.txt",
-            "1.0.0",
-            validate=True,
-        )
-    assert not os.path.exists("local.txt")
-    backend.get_file(
-        "/remote.txt",
-        "local.txt",
-        "1.0.0",
-        validate=True,
-    )
-    assert os.path.exists("local.txt")
+    def test_get_file(self, error_msg):
+        """Test validate for get_file."""
+        self.backend.put_file(self.local_file, "/remote.txt", validate=True)
+        with pytest.raises(InterruptedError, match=error_msg):
+            self.broken_backend.get_file("/remote.txt", "local.txt", validate=True)
+        assert not os.path.exists("local.txt")
+        self.backend.get_file("/remote.txt", "local.txt", validate=True)
+        assert os.path.exists("local.txt")
 
-    # copy_file()
-    with pytest.raises(InterruptedError, match=error_msg):
-        broken_backend.copy_file(
-            "/remote.txt",
-            "/copy.txt",
-            validate=True,
-        )
-    assert not backend.exists("/copy.txt", "1.0.0")
-    backend.copy_file(
-        "/remote.txt",
-        "/copy.txt",
-        version="1.0.0",
-        validate=True,
-    )
-    assert backend.exists("/copy.txt", "1.0.0")
+    def test_copy_file(self, error_msg):
+        """Test validate for copy_file."""
+        self.backend.put_file(self.local_file, "/remote.txt", validate=True)
+        with pytest.raises(InterruptedError, match=error_msg):
+            self.broken_backend.copy_file("/remote.txt", "/copy.txt", validate=True)
+        assert not self.backend.exists("/copy.txt")
+        self.backend.copy_file("/remote.txt", "/copy.txt", validate=True)
+        assert self.backend.exists("/copy.txt")
 
-    # move_file()
-    with pytest.raises(InterruptedError, match=error_msg):
-        broken_backend.move_file(
-            "/remote.txt",
-            "/move.txt",
-            version="1.0.0",
-            validate=True,
-        )
-    assert not backend.exists("/move.txt", "1.0.0")
-    assert backend.exists("/remote.txt", "1.0.0")
-    backend.move_file(
-        "/remote.txt",
-        "/move.txt",
-        version="1.0.0",
-        validate=True,
-    )
-    assert backend.exists("/move.txt", "1.0.0")
-    assert not backend.exists("/remote.txt", "1.0.0")
+    def test_move_file(self, error_msg):
+        """Test validate for move_file."""
+        self.backend.put_file(self.local_file, "/remote.txt", validate=True)
+        with pytest.raises(InterruptedError, match=error_msg):
+            self.broken_backend.move_file("/remote.txt", "/move.txt", validate=True)
+        assert not self.backend.exists("/move.txt")
+        assert self.backend.exists("/remote.txt")
+        self.backend.move_file("/remote.txt", "/move.txt", validate=True)
+        assert self.backend.exists("/move.txt")
+        assert not self.backend.exists("/remote.txt")
 
-    # put_archive()
-    with pytest.raises(InterruptedError, match=error_msg):
-        broken_backend.put_archive(
-            tmpdir,
-            "/remote.zip",
-            "1.0.0",
-            validate=True,
-        )
-    assert not backend.exists("/remote.zip", "1.0.0")
-    backend.put_archive(
-        ".",
-        "/remote.zip",
-        "1.0.0",
-        validate=True,
-    )
-    assert backend.exists("/remote.zip", "1.0.0")
+    def test_put_archive(self, error_msg):
+        """Test validate for put_archive."""
+        with pytest.raises(InterruptedError, match=error_msg):
+            self.broken_backend.put_archive(".", "/remote.zip", validate=True)
+        assert not self.backend.exists("/remote.zip")
+        self.backend.put_archive(".", "/remote.zip", validate=True)
+        assert self.backend.exists("/remote.zip")
 
-    # get_archive()
-    dst_root = os.path.join(tmpdir, "extract")
-    with pytest.raises(InterruptedError, match=error_msg):
-        broken_backend.get_archive(
-            "/remote.zip",
-            dst_root,
-            "1.0.0",
-            validate=True,
-        )
-    assert not os.path.exists(dst_root)
-    backend.get_archive(
-        "/remote.zip",
-        dst_root,
-        "1.0.0",
-        validate=True,
-    )
-    assert os.path.exists(dst_root)
+    def test_get_archive(self, error_msg):
+        """Test validate for get_archive."""
+        self.backend.put_archive(".", "/remote.zip", validate=True)
+        with pytest.raises(InterruptedError, match=error_msg):
+            self.broken_backend.get_archive("/remote.zip", "./extract", validate=True)
+        assert not os.path.exists("./extract")
+        self.backend.get_archive("/remote.zip", "./extract", validate=True)
+        assert os.path.exists("./extract")
