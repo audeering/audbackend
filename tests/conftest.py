29b5990e7349aa74136b7aa4c928539c9f202c77
Hagen Wierstorf <hwierstorf@audeering.com>
2024-10-18 19:55:40 +0200

    Revert "Fix owner test under Windows"
    This reverts commit 643c85d6f5b375f4d116d3e899bc96e41fe7e000.

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,7 +79,7 @@ def owner(request):
         owner = backend_cls.get_authentication(host_wo_https)[0]
     else:
         if os.name == "nt":
-            owner = "runneradmin"
+            owner = "Administrators"
         else:
             owner = getpass.getuser()
 
