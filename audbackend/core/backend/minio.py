3f0a4b08f65f08b694c1c72f850dd21c62f1fbc1
Hagen Wierstorf <hwierstorf@audeering.com>
2024-10-22 15:20:45 +0200

    Add exception in _get_file()

--- audbackend/core/backend/minio.py
+++ audbackend/core/backend/minio.py
@@ -288,6 +288,8 @@ class Minio(Base):
                             dst_fp.write(data)
                             dst_size += n_data
                             pbar.update(n_data)
+            except Exception as e:
+                raise RuntimeError(f"Error downloading file: {e}")
             finally:
                 response.close()
                 response.release_conn()
