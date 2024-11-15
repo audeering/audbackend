8632f15c6ee2b7243198b6a9cec593b9053884fa
Hagen Wierstorf <hwierstorf@audeering.com>
2024-11-15 13:44:40 +0100

    Add missing SkipParser

--- docs/conftest.py
+++ docs/conftest.py
@@ -3,6 +3,7 @@ from doctest import ELLIPSIS
 from sybil import Sybil
 from sybil.parsers.rest import DocTestParser
 from sybil.parsers.rest import PythonCodeBlockParser
+from sybil.parsers.rest import SkipParser
 
 from audbackend.core.conftest import mock_date  # noqa: F401
 from audbackend.core.conftest import mock_owner  # noqa: F401
@@ -10,7 +11,11 @@ from audbackend.core.conftest import prepare_docstring_tests  # noqa: F401
 
 
 pytest_collect_file = Sybil(
-    parsers=[DocTestParser(optionflags=ELLIPSIS), PythonCodeBlockParser()],
+    parsers=[
+        DocTestParser(optionflags=ELLIPSIS),
+        PythonCodeBlockParser(),
+        SkipParser(),
+    ],
     pattern="*.rst",
     fixtures=["mock_date", "mock_owner", "prepare_docstring_tests"],
 ).pytest()
