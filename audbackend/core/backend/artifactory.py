88dd39d79a1289169027e8d99d8f37c1d57f285c
Hagen Wierstorf <hwierstorf@audeering.com>
2025-10-23 10:23:52 +0200

    Revert "Move arguments to classes"
    This reverts commit b823bbe50a7fa4d99ab15b77ec88bd06a74c9918.

--- audbackend/core/backend/artifactory.py
+++ audbackend/core/backend/artifactory.py
@@ -75,12 +75,6 @@ class Artifactory(Base):
         authentication: username, password / API key / access token tuple.
             If ``None``,
             it requests it by calling :meth:`get_authentication`
-        num_workers: number of parallel jobs for downloads.
-            Not used for Artifactory backend.
-            Defaults to ``1``
-        chunk_size: chunk size in bytes for downloading.
-            If ``None``,
-            defaults to ``4096`` bytes
 
     """  # noqa: E501
 
@@ -90,16 +84,8 @@ class Artifactory(Base):
         repository: str,
         *,
         authentication: tuple[str, str] = None,
-        num_workers: int = 1,
-        chunk_size: int | None = None,
     ):
-        super().__init__(
-            host,
-            repository,
-            authentication=authentication,
-            num_workers=num_workers,
-            chunk_size=chunk_size,
-        )
+        super().__init__(host, repository, authentication=authentication)
 
         if authentication is None:
             self.authentication = self.get_authentication(host)
@@ -278,10 +264,13 @@ class Artifactory(Base):
         src_path: str,
         dst_path: str,
         verbose: bool,
+        num_workers: int,
+        chunk_size: int,
     ):
         r"""Get file from backend."""
         src_path = self.path(src_path)
-        chunk_size = self.chunk_size if self.chunk_size is not None else 4 * 1024
+        if chunk_size is None:
+            chunk_size = 4 * 1024
         _download(src_path, dst_path, chunk=chunk_size, verbose=verbose)
 
     def _ls(
