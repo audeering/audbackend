acf4db584751532802158c7ba40d3a0f6c40f612
Hagen Wierstorf <hwierstorf@audeering.com>
2025-11-10 08:59:37 +0000

    Update benchmark script

--- minio-parallel.py
+++ minio-parallel.py
@@ -23,51 +23,48 @@ import audbackend
 def main():
     host = "s3.dualstack.eu-north-1.amazonaws.com"
     repository = "audmodel-internal"
+    src_path = "/alm/audeering-omni/stage1_2/torch/7289b57d.zip"
+    version = "1.0.0"
     dst_path = "./tmp.zip"
     num_iter = 10
 
-    for name, src_path, version in [
-        ("50mb", "/alm/speechgpt/vocoder/torch/a384dab4.zip", "1.0.0"),
-        ("5gb", "/alm/speechgpt/torch/36f24586.zip", "1.0.0"),
-    ]:
-        ds = []
+    ds = []
 
-        for num_workers in audeer.progress_bar([1, 5, 10]):
-            backend = audbackend.backend.Minio(host, repository)
-            backend.open()
+    for num_workers in audeer.progress_bar([1, 2, 3, 4, 5, 10]):
+        backend = audbackend.backend.Minio(host, repository)
+        backend.open()
 
-            elapsed = []
+        elapsed = []
 
-            for _ in range(num_iter):
-                if os.path.exists(dst_path):
-                    os.remove(dst_path)
+        for _ in range(num_iter):
+            if os.path.exists(dst_path):
+                os.remove(dst_path)
 
-                interface = audbackend.interface.Maven(backend)
+            interface = audbackend.interface.Maven(backend)
 
-                t = time.time()
-                interface.get_file(
-                    src_path=src_path,
-                    dst_path=dst_path,
-                    version=version,
-                    num_workers=num_workers,
-                )
-                elapsed.append(time.time() - t)
-
-            ds.append(
-                {
-                    "backend": backend.__class__.__name__,
-                    "num_workers": num_workers,
-                    "num_iter": num_iter,
-                    "elapsed(avg)": str(datetime.timedelta(seconds=np.mean(elapsed))),
-                    "elapsed(std)": str(datetime.timedelta(seconds=np.std(elapsed))),
-                }
+            t = time.time()
+            interface.get_file(
+                src_path=src_path,
+                dst_path=dst_path,
+                version=version,
+                num_workers=num_workers,
             )
+            elapsed.append(time.time() - t)
 
-            backend.close()
+        ds.append(
+            {
+                "num_workers": num_workers,
+                "num_iter": num_iter,
+                "elapsed(avg)": str(datetime.timedelta(seconds=np.mean(elapsed))),
+                "elapsed(std)": str(datetime.timedelta(seconds=np.std(elapsed))),
+            }
+        )
 
-        df = pd.DataFrame(ds)
-        df.to_csv(f"results-{name}.csv", index=False)
-        df.to_markdown(f"results-{name}.md", index=False)
+        backend.close()
+
+    df = pd.DataFrame(ds)
+    df.to_csv(f"results.csv", index=False)
+    df.to_markdown(f"results.md", index=False)
 
 
 if __name__ == "__main__":
